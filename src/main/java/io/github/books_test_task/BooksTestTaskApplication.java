397d3752ac0243af8e7cc098a79f97c31430080d
LoucterSo <vlad_gorelin@inbox.ru>
2025-04-10 15:29:02 +0300

    Getting started

---
+++ src/main/java/io/github/books_test_task/BooksTestTaskApplication.java
@@ -0,0 +1,13 @@
+package io.github.books_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/books_test_task/BooksTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.books_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
