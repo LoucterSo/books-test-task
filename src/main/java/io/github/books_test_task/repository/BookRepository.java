21d9e4d857f66ffc7fa87335f5887dfb503cf61c
LoucterSo <vlad_gorelin@inbox.ru>
2025-04-10 18:19:09 +0300

    Book repository added

---
+++ src/main/java/io/github/books_test_task/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package io.github.books_test_task.repository;
+
+import io.github.books_test_task.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+}
