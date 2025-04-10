daa02439680b12c63beaf69a26c922ceffbb1919
LoucterSo <vlad_gorelin@inbox.ru>
2025-04-10 18:20:14 +0300

    SecurityConfiguration added

--- src/main/java/io/github/books_test_task/config/SecurityConfiguration.java
+++ src/main/java/io/github/books_test_task/config/SecurityConfiguration.java
@@ -2,16 +2,38 @@ package io.github.books_test_task.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config -> config
+                        .requestMatchers("/books").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
 
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
                 .username("John")
                 .password("{noop}pas1")
